f75a7a4c22ff654a67a753b07183a46c5764b602
Vanshjs <86555492+Vanshjs@users.noreply.github.com>
2022-01-06 18:26:02 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+import plotly.express as px
+import csv
+import numpy as np
+
+def plotFigure(data_path):
+    with open(data_path) as csv_file:
+        df=csv.DictReader(csv_file)
+        fig=px.scatter(df,x="sleep in hours", y="Coffee in ml")
+        fig.show()
+
+def getDataSource(data_path):
+    coffee_in_ml=[]
+    sleep_in_hour=[]
+    with open(data_path) as csv_file:
+        csv_reader=csv.DictReader(csv_file)
+
+        for row in csv_reader:
+            coffee_in_ml.append(float(row["Coffee in ml"]))
+            sleep_in_hour.append(float(row["sleep in hours"]))
+
+    return {"x" : sleep_in_hour, "y": coffee_in_ml}
+
+def findCorrelation(datasource):
+    correlation=np.corrcoef(datasource["x"], datasource["y"])
+
+    print("Correlation between Coffee in ml and sleep in hours: \n--->",correlation[0,1])
+
+def setup():
+    data_path="cups of coffee vs hours of sleep.csv"
+    datasource=getDataSource(data_path)
+    findCorrelation(datasource)
+    plotFigure(data_path)
+
+setup()
